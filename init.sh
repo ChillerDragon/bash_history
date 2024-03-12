58432eed25a38666cec7a96a4a9ea3bc00e29900
ChillerDragon <ChillerDragon@gmail.com>
2024-03-12 15:02:57 +0800

    Fix it being totally broken lol

--- init.sh
+++ init.sh
@@ -6,12 +6,17 @@
 #   source ~/Desktop/git/bash_history/init.sh
 #
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 __chiller_hist_flipper() {
+  if [ ! -d "$SCRIPT_DIR" ]
+  then
+    echo "ChillerDragon does not know enough bash to write a simple script"
+    return
+  fi
   local match
   match="$( (grep -v '^#' ~/.bash_history; cat "$SCRIPT_DIR/bash_history") | fzf)"
-  READLINE_LINE="$match"
+  [ "$match" != "" ] && READLINE_LINE="$match"
 }
 
 bind -x '"\C-r":"__chiller_hist_flipper"'
