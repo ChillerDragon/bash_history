f469d7408d48a782368ff7dcdb8a421bbe58800c
ChillerDragon <ChillerDragon@gmail.com>
2024-03-13 11:31:45 +0800

    Make it suck less

--- init.sh
+++ init.sh
@@ -14,8 +14,19 @@ __chiller_hist_flipper() {
     echo "ChillerDragon does not know enough bash to write a simple script"
     return
   fi
+  # write to history before searching
+  # this way we can also search in commands
+  # we just ran
+  # which is a common use case duh
+  history -a
+  local hist_file="${HISTFILE:-~/.bash_history}"
+  if [ ! -f "$hist_file" ]
+  then
+    echo "Error history file not found $hist_file"
+    return
+  fi
   local match
-  match="$( (grep -v '^#' ~/.bash_history; cat "$SCRIPT_DIR/bash_history") | fzf)"
+  match="$( (grep -v '^#' "$hist_file"; cat "$SCRIPT_DIR/bash_history") | fzf)"
   [ "$match" != "" ] && READLINE_LINE="$match"
 }
 
