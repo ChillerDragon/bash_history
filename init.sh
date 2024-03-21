cf108ac39d84947c2a16e959282fef3eeb50f91a
ChillerDragon <ChillerDragon@gmail.com>
2024-03-21 10:57:14 +0800

    Add ctrl+o for old reverse in case i need strict matches

--- init.sh
+++ init.sh
@@ -9,26 +9,30 @@
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 __chiller_hist_flipper() {
-  if [ ! -d "$SCRIPT_DIR" ]
-  then
-    echo "ChillerDragon does not know enough bash to write a simple script"
-    return
-  fi
-  # write to history before searching
-  # this way we can also search in commands
-  # we just ran
-  # which is a common use case duh
-  history -a
-  local hist_file="${HISTFILE:-~/.bash_history}"
-  if [ ! -f "$hist_file" ]
-  then
-    echo "Error history file not found $hist_file"
-    return
-  fi
-  local match
-  match="$( (tac "$hist_file"; cat "$SCRIPT_DIR/bash_history"; test -f "$SCRIPT_DIR"/nudes.txt && cat "$SCRIPT_DIR"/nudes.txt) | fzf)"
-  [ "$match" != "" ] && READLINE_LINE="$match"
-}
+	if [ ! -d "$SCRIPT_DIR" ]
+	then
+		echo "ChillerDragon does not know enough bash to write a simple script"
+		return
+	fi
+	# write to history before searching
+	# this way we can also search in commands
+	# we just ran
+	# which is a common use case duh
+	history -a
+	local hist_file="${HISTFILE:-~/.bash_history}"
+	if [ ! -f "$hist_file" ]
+	then
+		echo "Error history file not found $hist_file"
+		return
+	fi
+	local match
+	match="$( ( \
+		tac "$hist_file"; cat "$SCRIPT_DIR/bash_history"; test -f "$SCRIPT_DIR"/nudes.txt && cat "$SCRIPT_DIR"/nudes.txt \
+		) |
+		fzf --prompt='[C-o] for old> ')"
+			[ "$match" != "" ] && READLINE_LINE="$match"
+		}
 
 bind -x '"\C-r":"__chiller_hist_flipper"'
+bind '"\C-o":reverse-search-history'
 
