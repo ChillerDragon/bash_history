fbd94d81e8504595570026ea04b756a20e4f8880
ChillerDragon <ChillerDragon@gmail.com>
2024-03-23 10:57:07 +0800

    Justify bad code that is coping with config skill issues with a wall of text

--- init.sh
+++ init.sh
@@ -8,6 +8,24 @@
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
+# i have ignoreboth set for my bash history
+# but somehow shit does not work
+# so i uniq it my self
+# this might be causing issues and performance bottle necks
+# so if anything starts to get bugged
+# just nuke this function
+#
+# @param hist_file
+__chiller_hist_flipper_deduplicate() {
+	local hist_file="$1"
+	if ! awk '!x[$0]++' "$hist_file" > "$hist_file".tmp
+	then
+		return 1
+	fi
+	mv "$hist_file".tmp "$hist_file"
+	return 0
+}
+
 __chiller_hist_flipper() {
 	if [ ! -d "$SCRIPT_DIR" ]
 	then
@@ -25,6 +43,13 @@ __chiller_hist_flipper() {
 		echo "Error history file not found $hist_file"
 		return
 	fi
+
+	if ! __chiller_hist_flipper_deduplicate "$hist_file"
+	then
+		echo "Error failed to deduplicate $hist_file"
+		return
+	fi
+
 	local match
 	match="$( ( \
 		tac "$hist_file"; cat "$SCRIPT_DIR/bash_history"; test -f "$SCRIPT_DIR"/nudes.txt && cat "$SCRIPT_DIR"/nudes.txt \
